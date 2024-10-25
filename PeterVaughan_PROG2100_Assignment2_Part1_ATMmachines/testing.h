d5530523b11f362cc79a8355f9de05e6eb0278dd
Peter Vaughan <160742536+cadalac-don@users.noreply.github.com>
2024-10-25 10:36:02 -0300

    Part A

---
+++ PeterVaughan_PROG2100_Assignment2_Part1_ATMmachines/testing.h
@@ -0,0 +1,33 @@
+//
+// Created by vaugh on 2024-10-25.
+//
+
+#ifndef TESTING_H
+#define TESTING_H
+
+#endif //TESTING_H
+
+void testing() {
+    /*
+    //random lines for troubleshooting my code and the values
+
+    //See the variables that are currently added
+    cout << endl << accountNumber << endl;
+    cout << firstName << endl;
+    cout << lastName << endl;
+    cout << password << endl;
+    printf("%.2f", bankBalance);
+
+    //I was using char instead of string and it was giving me problems
+    cout << sizeof(accountNumber) << endl;
+    cout << sizeof(firstName) << endl;
+    cout << sizeof(lastName) << endl;
+    cout << sizeof(password) << endl;
+    */
+
+    //current output for various stages
+    cout << accountNumber << endl;
+    cout << firstName << endl;
+    cout << lastName << endl;
+    cout << password << endl;
+}
